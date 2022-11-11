2dac88f64ef10c9e52dc43f9f844a82906037970
Takuya Shimada <gct3860@gmail.com>
2022-11-11 15:23:44 +0900

    Update Format of Summary Table (#20)
    * update format
    
    * Apply automatic changes
    
    * minor update
    
    * minor update
    
    * Apply automatic changes
    
    * minor update
    
    * Apply automatic changes
    
    * center align
    
    * Apply automatic changes
    
    * sort by problem number
    
    * Apply automatic changes
    
    Co-authored-by: 29Takuya <29Takuya@users.noreply.github.com>

--- generate_summary.py
+++ generate_summary.py
@@ -1,9 +1,15 @@
+import collections
 import pathlib
 import re
 import typing
 
 ROOTDIR = pathlib.Path(__file__).parent
 DIFFICULTIES = typing.Literal["easy", "medium", "hard"]
+LANG = {
+    "py": "Python",
+    "java": "Java",
+    "kt": "Kotlin",
+}
 
 
 class Problem(typing.NamedTuple):
@@ -25,22 +31,24 @@ for difficulty in typing.get_args(DIFFICULTIES):
         title = title.replace("-", " ")
         problems.append(Problem(number, title, path, difficulty))
 
-# Sort by problem title (i.e. problem number)
-problems.sort(key=lambda x: x.path.name)
+problems.sort(key=lambda x: x.number)
 
 contents = []
 contents.append("# Leetcode\n")
-contents.append("| # | Title | Solution | Difficulty |\n")
-contents.append("| ------ | ------ | ------ | ------ |\n")
+contents.append("| # | Title | Solutions | Difficulty |\n")
+contents.append("| :------: | ------ | :------: | :------: |\n")
 
 for problem in problems:
-    solutions = []
+    solutions = collections.defaultdict(list)
     for sol_path in sorted(problem.path.glob("*")):
-        # Add file name with relative link.
-        solutions.append(
-            f"[{sol_path.name}]({str(sol_path.relative_to(ROOTDIR))})")
+        name, extension = sol_path.name.split(".")
+        solutions[name].append(
+            f"[{LANG[extension]}]({str(sol_path.relative_to(ROOTDIR))})")
+
+    solutions_str = "</br>".join(
+        (f"{name} ({', '.join(elements)})" for name, elements in solutions.items()))
     contents.append(
-        f"| {problem.number} | {problem.title} | {'</br>'.join(solutions)} | {problem.difficulty} | " + "\n")
+        f"| {problem.number} | {problem.title} | {solutions_str} | {problem.difficulty} | " + "\n")
 
 with open("README.md", mode="w") as f:
     f.writelines(contents)
